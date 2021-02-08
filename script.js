621ef28dda8c6bf0f54d021e41680467a4f656de
bas132828 <63089699+bas132828@users.noreply.github.com>
2021-02-08 17:35:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -277,9 +277,36 @@ labelBalance.addEventListener("click", function () {
 // console.log(new Date(2037, 9, 19, 15, 23, 5));
 // console.log(new Date(100000000000000));
 
-const future = new Date(2037, 9, 19, 15, 23);
-console.log(future);
+// const future = new Date(2037, 9, 19, 15, 23);
+// console.log(future);
+//
+// console.log(future.getFullYear());
+// console.log(future.getMonth());
+// // console.log(future.getDate());
+// let d1 = 0;
+// let d2 = 0;
+//  // [[1,2,3],[4,5,6],[7,8,9]]
+//  debugger;
+// const y = function diagonalDifference(arr) {
+//   let [d1, d2] = [0,0];
+//   for (let i = 0; i<arr.length; i++) {
+//   d1+= arr[i][i];
+//   d2+= arr[i][arr.length - i -1]};
+//   return Math.abs(d1+d2);
+// }
+//
+//
+// console.log(y([[11,2,4],[4,5,6],[10,8,-12]]));
+// d1+=arr[i][i];
+//   d2+=arr[ar.length][i.length-1];
 
-console.log(future.getFullYear());
-console.log(future.getMonth());
-console.log(future.getDate());
+function staircase(n) {
+let i=1;
+while (i<=n){
+console.log(`${'#'.repeat(i).padStart(n, ' ')}`)
+i++;
+};
+//.padStart(n, ' ')
+}
+
+console.log(staircase(6))
